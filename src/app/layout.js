5ae3f6809146292e84c01cecb9194ff345b826d4
ali husnain <bakhtawaralishigri@gmail.com>
2025-06-05 13:11:56 +0500

    feat: configer email

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,8 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
    <html lang="en">
-    <body className={inter.className}>
+      <body className={inter.className}>
+        {/* AuthProvider */}
      <AuthProvider>
       <Header />
       {children}
